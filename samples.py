fe30ca6499f52efb8ad883b4227c98cf41078190
ukaznil <ukaznil@gmail.com>
2022-04-10 23:24:33 -0500

    replace a sample in LooksRare

--- samples.py
+++ samples.py
@@ -123,7 +123,7 @@ def sample_looksrare():
     print(r.fetch(Market.LooksRare, '0x7Bd29408f11D2bFC23c34f18275bBf23bB716Bc7'))  # Meebits
     print(r.fetch(Market.LooksRare, '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'))  # Bored Ape Yacht Club
     print(r.fetch(Market.LooksRare, '0x60E4d786628Fea6478F785A6d7e704777c86a7c6'))  # Mutant Ape Yacht Club
-    print(r.fetch(Market.LooksRare, '0xbC578eCcA2115dAC0c93C08674edC0C7D01Fe09C'))  # InvisibleSociety
+    print(r.fetch(Market.LooksRare, '0x7a376C1F8eDF7BA7Eb94A7438a8313a17D830FF8'))  # ProjectKevins
     print(r.fetch(Market.LooksRare, '0xED5AF388653567Af2F388E6224dC7C4b3241C544'))  # Azuki
     print(r.fetch(Market.LooksRare, '0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B'))  # CloneX
     print(r.fetch(Market.LooksRare, '0xBd785591D714f691E939de7ec6D72550a44c598c'))  # AlphaDoggg Tag
