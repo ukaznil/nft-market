2181ec5fb1cc4c4e549b243677c36a6c94699157
ukaznil <ukaznil@gmail.com>
2022-05-01 21:40:54 -0500

    release 1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nft_market",
-    version="1.9",
+    version="1.10",
     author="ukaznil",
     author_email="ukaznil@gmail.com",
     description="NFT market is in your hands.",
