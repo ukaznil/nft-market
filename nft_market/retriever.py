e48468d30e6fd05eb8f8f7134890180fab134e9d
ukaznil <ukaznil@gmail.com>
2022-07-08 22:35:59 -0500

    fix opensea

--- nft_market/retriever.py
+++ nft_market/retriever.py
@@ -151,8 +151,8 @@ class Retriever:
                     .name('//*[@id="main"]/div/div/div[3]/div/div/div[1]/div/div[2]/h1') \
                     .num_supply(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[2]/button/div/span[1]/div') \
                     .num_owners(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[4]/a/div/span[1]/div') \
-                    .floor(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[6]/a/div/span[1]/div') \
-                    .volume(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[8]/a/div/span[1]/div') \
+                    .floor(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[8]/a/div/span[1]/div') \
+                    .volume(f'//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[6]/a/div/span[1]/div') \
                     .build()
             except Exception as e:
                 error = e
