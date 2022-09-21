1788d082cc3a2b3b84d6848f422b182fc946d46c
ukaznil <ukaznil@gmail.com>
2022-09-20 20:24:13 -0500

    deprecate NFTrade and Coinbase in tests

--- tests/test_market.py
+++ tests/test_market.py
@@ -53,10 +53,13 @@ def test_gem():
 
 
 def test_nftrade():
+    pass
+    '''
     # NFTrade
     print(r.fetch(Market.NFTrade, 'polygon/0xc93c53de60d1a28df01e41f5bc04619039d2ef4f'))  # League of Kingdoms Skin
     print(r.fetch(Market.NFTrade, 'bsc/0x8815fae8feb5e1b2f8a6c7c948d9fd1866e07a4f'))  # Mines of Dalarnia Land Plots
     print(r.fetch(Market.NFTrade, 'avalanche/0xaab56a5a22db41d6663aab0f8f0bed979c84c569'))  # PIRATE CHEST v2
+    '''
 
 
 def test_solanart():
@@ -87,10 +90,13 @@ def test_xanalia():
 
 
 def test_coinbase():
+    pass
+    '''
     # Coinbase
     print(r.fetch(Market.Coinbase, 'ethereum/0x23581767a106ae21c074b2276D25e5C3e136a68b'))  # Moonbirds
     print(r.fetch(Market.Coinbase, 'ethereum/0x60E4d786628Fea6478F785A6d7e704777c86a7c6'))  # Mutant Ape Yacht Club
     print(r.fetch(Market.Coinbase, 'ethereum/0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'))  # Bored Ape Yacht Club
+    '''
 
 
 def test_niftygateway():
